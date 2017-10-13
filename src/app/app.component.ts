a446bd8000b3a6b36531b0d6a700a7a00fa515d4
William Lohan <w.lohan@tabsautobiz.com>
2017-10-13 13:31:57 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, Compiler, NgModule, OnInit } from '@angular/core';
+import { JitCompilerFactory } from '@angular/compiler';
+
+export function CustomComponent(annotation: any) {
+  return function (target: Function) {
+    const metaData = new Component(annotation);
+    Component(metaData)(target);
+  };
+}
+
+export function CustomNgModule(annotation: any) {
+  return function (target: Function) {
+    const metaData = new NgModule(annotation);
+    NgModule(metaData)(target);
+  };
+}
+
+export function compilerFactory() {
+  return new JitCompilerFactory([{ useDebug: false, useJit: true }]).createCompiler();
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: Compiler, useFactory: compilerFactory }]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public template = '<div>Hello JIT</div>';
+
+  constructor(
+    private view: ViewContainerRef,
+    private compiler: Compiler
+  ) { }
+
+  private createCompoent(template: string) {
+    const metadata = {
+      selector: 'dynamic-component',
+      template: template
+    };
+
+    @Component(metadata)
+    @CustomComponent(metadata)
+    class DynamicComponent { }
+
+    return DynamicComponent;
+  }
+
+  private createModule(modules: any[], componentType: any) {
+    const metadata = {
+      imports: [...modules],
+      declarations: [componentType]
+    };
+
+    @NgModule(metadata)
+    @CustomNgModule(metadata)
+    class DynamicModule { }
+
+    return DynamicModule;
+  }
+
+  public async ngOnInit() {
+    const compoent = this.createCompoent(this.template);
+    const module = this.createModule([], compoent);
+    const moduleWithComponentFactories = await this.compiler.compileModuleAndAllComponentsAsync(module);
+    const compoentFactory = moduleWithComponentFactories.componentFactories[0];
+    this.view.clear();
+    const compRef = this.view.createComponent(compoentFactory);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
+import { NgModule, Compiler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +9,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
